1bdf6a4f972e460ba1fc52f4c0b50cada822d7fc
ray472809026 <ray472809026@hotmail.com>
2022-01-30 13:39:16 +0800

    Add build-gh script

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,50 @@
 const path = require('path')
-const { entryPoints, HTMLWebpackPlugins } = require('./webpakc-config/entry-point')
 const htmlRule = require('./webpakc-config/htmlRule')
 const { jsRule, tsRule } = require('./webpakc-config/jsRules')
 const { cssRule, lessRule, sassRule, miniCssExtractPlugin } = require('./webpakc-config/css-rules')
 const { imgRule, fontRule } = require('./webpakc-config/asset-module')
 
-module.exports = {
-  entry: {
-    ...entryPoints,
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].js',
-    clean: true,
-    // environment: { // Compatible for IE, equals "target: ['web', 'es5']" below
-    //   arrowFunction: false,
-    //   const: false
-    // }
-  },
-  optimization: {
-    splitChunks: { // extract common dependencies
-      chunks: 'all',
+module.exports = env => {
+  const getEntryPoint = require('./webpakc-config/entry-point')
+  const { entryPoints, HTMLWebpackPlugins } = getEntryPoint(env.gh)
+
+  return {
+    entry: {
+      ...entryPoints,
     },
-  },
-  target: ['web', 'es5'],
-  module: {
-    rules: [
-      htmlRule,
-      jsRule,
-      tsRule,
-      cssRule,
-      lessRule,
-      sassRule,
-      imgRule,
-      fontRule,
-    ]
-  },
-  plugins: [
-    ...HTMLWebpackPlugins,
-    miniCssExtractPlugin,
-  ],
-  resolve: {
-    extensions: ['.ts', '.js']
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/[name].js',
+      clean: true,
+      // environment: { // Compatible for IE, equals "target: ['web', 'es5']" below
+      //   arrowFunction: false,
+      //   const: false
+      // }
+    },
+    optimization: {
+      splitChunks: { // extract common dependencies
+        chunks: 'all',
+      },
+    },
+    target: ['web', 'es5'],
+    module: {
+      rules: [
+        htmlRule,
+        jsRule,
+        tsRule,
+        cssRule,
+        lessRule,
+        sassRule,
+        imgRule,
+        fontRule,
+      ]
+    },
+    plugins: [
+      ...HTMLWebpackPlugins,
+      miniCssExtractPlugin,
+    ],
+    resolve: {
+      extensions: ['.ts', '.js']
+    }
   }
 }
--- webpakc-config/entry-point.js
+++ webpakc-config/entry-point.js
@@ -1,39 +1,48 @@
-const path = require('path')
-const fs = require('fs');
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+function getEntryPoint(gh) {
+  const path = require('path')
+  const fs = require('fs');
+  const HTMLWebpackPlugin = require('html-webpack-plugin')
 
-const srcPath = path.join(__dirname, '../src');
-const entryPoints = {}
-const HTMLWebpackPlugins = []
+  const srcPath = path.join(__dirname, '../src');
+  const entryPoints = {}
+  const HTMLWebpackPlugins = []
 
-try {
-  let filenames = fs.readdirSync(srcPath)
-  filenames = filenames.filter(file => path.extname(file) === '.html')
+  try {
+    let filenames = fs.readdirSync(srcPath)
+    filenames = filenames.filter(file => path.extname(file) === '.html')
 
-  filenames.forEach(filename => {
-    const baseName = path.basename(filename, '.html')
+    filenames.forEach(filename => {
+      const baseName = path.basename(filename, '.html')
 
-    if (fs.existsSync(`${srcPath}/js/${baseName}.js`)) {
-      entryPoints[baseName] = `${srcPath}/js/${baseName}.js`
-    } else if (fs.existsSync(`${srcPath}/js/${baseName}.ts`)) {
-      entryPoints[baseName] = `${srcPath}/js/${baseName}.ts`
+      if (fs.existsSync(`${srcPath}/js/${baseName}.js`)) {
+        entryPoints[baseName] = `${srcPath}/js/${baseName}.js`
+      } else if (fs.existsSync(`${srcPath}/js/${baseName}.ts`)) {
+        entryPoints[baseName] = `${srcPath}/js/${baseName}.ts`
+      }
+
+      const options = {
+        filename: `${baseName}.html`,
+        template: `${srcPath}/${baseName}.html`,
+        chunks: [`${baseName}`],
+      }
+
+      if (gh) {
+        options.base = { 'href': 'https://ray1413.github.io/webpack-multi-page/dist/' }
+      }
+
+      HTMLWebpackPlugins.push(new HTMLWebpackPlugin(options))
+    })
+
+
+    return {
+      entryPoints,
+      HTMLWebpackPlugins
     }
-
-    HTMLWebpackPlugins.push(new HTMLWebpackPlugin({
-      filename: `${baseName}.html`,
-      template: `${srcPath}/${baseName}.html`,
-      chunks: [`${baseName}`],
-      base: { 'href': 'https://ray1413.github.io/webpack-multi-page/dist/' },
-    }))
-
-  })
-
-  // console.log(entryPoints)
-} catch (e) {
-  console.log(e)
+    // console.log(entryPoints)
+  } catch (e) {
+    console.log(e)
+    return {}
+  }
 }
 
-module.exports = {
-  entryPoints,
-  HTMLWebpackPlugins
-}
+module.exports = getEntryPoint
