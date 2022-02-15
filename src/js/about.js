4b35f6404e72f42250121b4c5d8751cf4ee510bb
ray472809026 <ray472809026@hotmail.com>
2022-02-15 17:20:46 +0800

    🚦Add 'github-markdown-css' Highlight code blocks using Highlight.js

--- src/js/about.js
+++ src/js/about.js
@@ -2,16 +2,24 @@ import 'bootstrap'
 import '../css/bootstrap.sass'
 import './common/navbar'
 import readme from '../../README.md'
+import 'github-markdown-css'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/vs.css'
 
 const body = document.querySelector('body')
 const mdContainer = document.createElement('div')
-const mdTitle = document.createElement('div')
-const mdContent = document.createElement('div')
 
-mdTitle.innerText = 'README.md'
-mdTitle.classList.add('border-bottom', 'mb-2', 'pb-2')
-mdContent.innerHTML = readme
-mdContainer.append(mdTitle, mdContent)
-mdContainer.classList.add('container', 'mt-5', 'p-3', 'bg-white', 'rounded')
+mdContainer.innerHTML = `
+<div class='container mt-5 p-3 rounded' style='max-width: 980px;background-color: white;'>
+  <div class='border-bottom mb-2 pb-2'>README.md</div>
+  <div class='markdown-body'>${readme}</div>
+</div>
+`
+
+mdContainer.querySelectorAll('pre code').forEach((el) => {
+  if (el.className.indexOf('language-') >= 0) {
+    hljs.highlightElement(el)
+  }
+})
 
 body.append(mdContainer)
